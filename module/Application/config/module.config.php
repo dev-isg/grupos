e4427afe0c03d5fa0803d6b3827bfbd5a581e3df
Kevin Castillo <kcastillo@innovationssystems.com>
2013-07-31 16:07:59 -0500

    integracion_grupos

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -15,7 +15,7 @@ return array(
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
+                        'controller' => 'Grupo\Controller\Index',//'Application\Controller\Index',
                         'action'     => 'index',
                     ),
                 ),
