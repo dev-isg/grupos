88b834fdc59445ef26bc082219a7f82465c799cc
Josmel Yupanqui <yyupanqui@innovationssystems.com>
2013-08-14 12:49:58 -0500

    subiendo cambios

--- module/Grupo/src/Grupo/Controller/IndexController.php
+++ module/Grupo/src/Grupo/Controller/IndexController.php
@@ -287,12 +287,14 @@ class IndexController extends AbstractActionController
         $eventospasados=$this->getEventoTable()->eventospasados($id);
         $eventosfuturos=$this->getEventoTable()->eventosfuturos($id);
         $usuarios=$this->getGrupoTable()->usuariosgrupo($id);
-      
+        $proximos_eventos=$this->getGrupoTable()->eventosgrupo($id);
+        //var_dump($eventos);exit;
        return array(
                      'grupo'=>$grupo,
             'eventosfuturos'=>$eventosfuturos,
             'eventospasados'=>$eventospasados,
                   'usuarios'=> $usuarios,
+                  'proximos_eventos' =>$proximos_eventos,
          );
     }
     
