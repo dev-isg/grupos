a9831e5a95ae3620d37e0770968fe00e5946b92b
Kevin Castillo <kcastillo@innovationssystems.com>
2013-08-16 09:26:10 -0500

    cambios_asistencia

---
+++ module/Grupo/src/Grupo/Form/ComentarioForm.php
@@ -0,0 +1,53 @@
+<?php
+namespace Grupo\Form;
+
+use Zend\Form\Form;
+use Zend\InputFilter\InputFilterProviderInterface;
+use Zend\InputFilter\InputFilter;
+
+class ComentarioForm extends Form
+{
+     public function __construct($name = null)
+    {
+        parent::__construct('comentario');
+        $this->setAttribute('method', 'post');
+   
+         $this->add(array(
+            'name' => 'va_descripcion',
+            'type' => 'TextArea',
+             'attributes' => array(
+                 'class' => '',
+                 'id' => 'texto-comentar',
+                 'placeholder' => 'Ingrese su comentario aquí.',
+                 'cols' => 30,
+                 'rows' => 5
+            ),
+        ));  
+
+        $this->add(array(
+            'name' => 'submit',
+            'type' => 'Submit',
+            'attributes' => array(
+                'value' => 'Ingresar',
+                'class' => 'btn btn-primary'
+            ),
+        ));
+        
+        $this->setInputFilter($this->validadores());
+    }
+    
+    public function validadores(){
+        $inputFilter = new InputFilter();
+        
+        $inputFilter->add(array(
+            'name' => 'va_descripcion',
+            'required' => true,
+        ));
+        
+        
+        
+        return $inputFilter;
+        
+    }
+}
+
---
+++ module/Usuario/src/Usuario/Form/NotificacionForm.php
@@ -0,0 +1,55 @@
+<?php
+namespace Usuario\Form;
+
+use Zend\Form\Form;
+use Zend\InputFilter\InputFilterProviderInterface;
+use Zend\InputFilter\InputFilter;
+
+
+class NotificacionForm extends Form
+{
+     public function __construct($name = null)
+    {
+        parent::__construct('cambio');
+        $this->setAttribute('method', 'post');
+        $this->setAttribute('endtype', 'multipart/form-data');
+   
+               $this->add(array(
+                   'name' => 'tipo_notificacion',
+                   'type' => 'MultiCheckbox',
+                    'attributes' => array(
+                        'value' => array('0', '1'),
+                       'class' => 'checkbox inline',
+                       'id'   => 'en_destaque',
+                        'placeholder'=>'Ingrese su destaque'
+                   ),
+                   'options' => array(
+                         'value_options' => array(
+                            '0'=>'Recibir avisos por email cuando ingresa al Grupo ',
+                            '1'=>'Recibir avisos por email cuando sale del Grupo',
+                         ),
+                       ),
+                   
+               ));
+         
+
+        $this->add(array(
+            'name' => 'submit',
+            'type' => 'Submit',
+        ));
+        $this->setInputFilter($this->validadores());
+    }
+    public function validadores(){
+    
+        $inputFilter = new InputFilter();
+  
+        $inputFilter->add(array(
+            'name' => 'tipo_notificacion',
+            'required' => false,
+
+        ));
+         
+    
+        return $inputFilter;
+    }
+}
