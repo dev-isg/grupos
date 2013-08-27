9f2491048b09ef3bd582f873d220a7ebd30e2dbb
Kevin Castillo <kcastillo@innovationssystems.com>
2013-08-27 14:42:39 -0500

    cambios_01

--- module/Grupo/src/Grupo/Controller/IndexController.php
+++ module/Grupo/src/Grupo/Controller/IndexController.php
@@ -200,7 +200,7 @@ class IndexController extends AbstractActionController
         $request = $this->getRequest();
         
         $urlorigen=$this->getRequest()->getHeader('Referer')->uri()->getPath();
-        
+       
         if ($request->isPost()) {
 
             $File = $this->params()->fromFiles('va_imagen');
@@ -234,7 +234,6 @@ class IndexController extends AbstractActionController
                    $idgrupo=$this->getGrupoTable()->guardarGrupo($grupo, $notificacion, $storage->read()->in_id,$imagen);
                    $this->flashMessenger()->addMessage('Su grupo ha sido registrado correctamente');
                    if($this->params()->fromPost('url')=='/usuario/index/misgrupos'){
-//                       var_dump($urlorigen);exit;
                        return $this->redirect()->toRoute('detalle-grupo',array('in_id'=>$idgrupo));
                        
                    }else{
