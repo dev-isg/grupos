151f72677ab69e3c583bdcbc28827043fe122bfa
jesser pariona huarcaya <jerspr@gmail.com>
2013-08-27 16:59:52 -0500

    update

--- public/js/ckeditor/config.js
+++ public/js/ckeditor/config.js
@@ -36,5 +36,5 @@ CKEDITOR.editorConfig = function( config ) {
 	// Make dialogs simpler.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 
-	//config.entities = false;
+	config.entities = false;
 };
