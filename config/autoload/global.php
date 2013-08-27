677742f654467052008fc07198a17b09bd467c85
Josmel Yupanqui <yyupanqui@innovationssystems.com>
2013-08-27 10:48:42 -0500

    cambios

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -31,15 +31,15 @@ return array(
         'secret' => '123456789123456789123456789'
     ),
     
-//   'host' => array(
-//
-//        'base' => 'http://192.168.1.50:82',
-//        'static' => 'http://192.168.1.50:82',
-//        'images' => 'http://192.168.1.50:82/imagenes',
-//        'img'=>'http://192.168.1.50:82/img',
-//        'ruta' => 'http://192.168.1.50:82',
-//        'version'=>1,
-//    ),
+   'host' => array(
+
+        'base' => 'http://192.168.1.37:82',
+        'static' => 'http://192.168.1.37:82',
+        'images' => 'http://192.168.1.37:82/imagenes',
+        'img'=>'http://192.168.1.37:82/img',
+        'ruta' => 'http://192.168.1.37:82',
+        'version'=>1,
+    ),
     
         'mail' => array(
         'transport' => array(
