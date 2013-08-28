eb2aefd82ab64ca82d661fb0b4c37625f6ba7e9a
Josmel Yupanqui <yyupanqui@innovationssystems.com>
2013-08-28 17:30:56 -0500

    cambios

--- module/SanAuth/config/module.config.php
+++ module/SanAuth/config/module.config.php
@@ -26,7 +26,7 @@ return array(
                     'process' => array(
                         'type'    => 'Segment',
                         'options' => array(
-                            'route'    => '/[/:action]',//[:controller
+                            'route'    => '/[/:action[/:in_id_face]]',//[:controller
                             'constraints' => array(
                                 'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
                                 'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
