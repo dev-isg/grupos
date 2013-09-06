be0d15d198ca782679e8d918281edac8d23890e3
Josmel Yupanqui <yyupanqui@innovationssystems.com>
2013-09-06 14:25:53 -0500

    cambios

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -16,7 +16,6 @@ return array(
 //   'facebook' => array(
 //        'appId' => '171038663080276',
 //        'secret' => '5739ee928ea9149e62096e9bb91c501f',
-//        'pass'=>'123456789159753'
 //    ),
     
      'host' => array(
