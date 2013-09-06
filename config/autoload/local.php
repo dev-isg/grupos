2d284d544d2815154bfb04b429d88517cf34fdfb
jesser pariona huarcaya <jerspr@gmail.com>
2013-09-05 19:05:25 -0500

    update

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -20,13 +20,6 @@ return array(
 //    ),
 //    
 
-   'host' => array(
-        'base' => 'http://dev.juntate.pe',
-        'static' => 'http://dev.juntate.pe',
-        'images' => 'http://192.168.1.50:82/imagenes',
-        'img'=>'http://192.168.1.50:82/img',
-        'ruta' => 'http://dev.juntate.pe',
-        'version'=>1,
-    ),
+
 
 );
