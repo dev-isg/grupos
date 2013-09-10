eb2980ae886baa881f1f1387178a35a3ffe3e7b6
Kevin Castillo <kcastillo@innovationssystems.com>
2013-09-10 14:49:26 -0500

    cambios_01

--- module/Grupo/src/Grupo/Controller/IndexController.php
+++ module/Grupo/src/Grupo/Controller/IndexController.php
@@ -917,8 +917,7 @@ class IndexController extends AbstractActionController
                        $loginUrl = $facebook->getLoginUrl(array('scope'=>'email,publish_stream,read_friendlists',  
                           'redirect_uri'=>  $this->_options->host->ruta.'/'
                            
-                           )); 
-                       return $this->redirect()->toUrl($this->getRequest()->getBaseUrl().'/');    
+                           ));     
                        }   
                  return array(
             'user_profile' => $user_profile,
