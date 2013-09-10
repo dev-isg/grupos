fef1ed83db4b719848fff259fd9998fe9bba57c1
Josmel Yupanqui <yyupanqui@innovationssystems.com>
2013-09-09 19:44:03 -0500

    ds

--- module/Grupo/src/Grupo/Controller/IndexController.php
+++ module/Grupo/src/Grupo/Controller/IndexController.php
@@ -909,7 +909,7 @@ class IndexController extends AbstractActionController
                              } 
                       else {
                        $loginUrl = $facebook->getLoginUrl(array('scope'=>'email,publish_stream,read_friendlists',  
-                           'redirect_uri'=>  $this->_options->host->ruta.'/'
+                           'redirect_uri'=>  $this->_options->host->ruta.'/registrarse/'
                            ));    }   
                  return array(
             'user_profile' => $user_profile,
