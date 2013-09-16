58a40428dfcab4ee7e5e493de7ad272835695a79
Kevin Castillo <kcastillo@innovationssystems.com>
2013-09-16 10:35:03 -0500

    comentarios_01

--- module/Grupo/src/Grupo/Model/EventoTable.php
+++ module/Grupo/src/Grupo/Model/EventoTable.php
@@ -225,9 +225,9 @@ class EventoTable{
              
          }else{   
              //agrege al grupo cuando se une al evento
-//             $evento=$this->getEvento($idevent);
-//             $idgrup=$evento->ta_grupo_in_id;
-//             $this->unirseGrupo($idgrup,$iduser);   
+             $evento=$this->getEvento($idevent);
+             $idgrup=$evento->ta_grupo_in_id;
+             $this->unirseGrupo($idgrup,$iduser);   
              $consulta = $this->tableGateway->getSql()->insert()->into('ta_usuario_has_ta_evento')
              ->values(array('ta_usuario_in_id'=>$iduser,'ta_evento_in_id'=>$idevent,'va_estado'=>'activo','va_fecha'=>date('c')));
          }
