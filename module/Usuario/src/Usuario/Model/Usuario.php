6ab0d2071018e8afa0572d4fc97dceb56b691014
Kevin Castillo <kcastillo@innovationssystems.com>
2013-09-18 15:08:38 -0500

    cambios_01

--- module/Usuario/src/Usuario/Model/Usuario.php
+++ module/Usuario/src/Usuario/Model/Usuario.php
@@ -342,7 +342,7 @@ class Usuario implements InputFilterAwareInterface{
 //                    ),
                     array(
                         'name'    => 'filesize',
-                        'options' =>  array('max' => 204800),
+                        'options' =>  array('max' =>'204800kB'),//
                     ),
                   ),
                ))
