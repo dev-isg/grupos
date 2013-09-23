9c08485ee690b6a7337b173e94bbdf1335b391a5
Josmel Yupanqui <yyupanqui@innovationssystems.com>
2013-09-23 18:31:17 -0500

    asas

--- module/Usuario/src/Usuario/Controller/IndexController.php
+++ module/Usuario/src/Usuario/Controller/IndexController.php
@@ -445,9 +445,9 @@ public function getAuthService() {
 
         $request = $this->getRequest();
 
-        if ($request->isPost()) {
-            $datos =$this->request->getPost();
-              var_dump($datos->ta_ubigeo_in_id);exit;
+        if ($request->isPost()) {    
+         //$datos =$this->request->getPost();
+        
             $File = $this->params()->fromFiles('va_foto');
           
             $nonFile = $this->params()->fromPost('va_nombre');
@@ -463,6 +463,7 @@ public function getAuthService() {
                 $filtered = $filter->filter($nom);
                 $imagen = $filtered . '-' . $imf2;
             } else {
+                              
                 $idusuario = $this->getUsuarioTable()->getUsuario($id);
                 $imagen = $idusuario->va_foto;
             }
@@ -472,15 +473,14 @@ public function getAuthService() {
                             ->toArray(), $this->getRequest()
                             ->getFiles()
                             ->toArray());
+           
             $form->setInputFilter($usuario->getInputFilter2());
-            $form->setData($data);
-            
-            
-//             $adapter = new \Zend\File\Transfer\Adapter\Http();
-//             $adapter->setValidators($adapter,$File['name']);
+//         
+          $form->setData($data);
+
  
             if ($form->isValid()) {
-                $catg_ingresada=$this->params()->fromPost('select2');
+            $catg_ingresada=$this->params()->fromPost('select2');
                 if ($this->params()->fromPost('va_contrasena') == '') {
                     $dataa = $this->getUsuarioTable()->getUsuario($id);
                     $pass = $dataa->va_contrasena;
